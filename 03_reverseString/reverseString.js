7a799124177440efd2669e7514900a7d866e5e0a
estellel-github <131867732+estellel-github@users.noreply.github.com>
2024-06-10 17:05:37 +0200

    Finish exercises 02_ and 03_

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let stringArray = string.split("");
+  let invertedArray = stringArray.reverse();
+  let invertedString = invertedArray.join("");
+  
+  return invertedString;
 };
+
+console.log(reverseString("Hello"));
+console.log(reverseString("hello there"));
+console.log(reverseString("123! abc! Hello, Odinite."));
+console.log(reverseString(""));
 
 // Do not edit below this line
 module.exports = reverseString;
