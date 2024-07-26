8f78f3ee3d078623eaa5cc1ef0d09955f4c9f1c3
estellel-github <131867732+estellel-github@users.noreply.github.com>
2024-07-26 16:04:19 +0200

    Sync exercises 08 09 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  word = word.toLowerCase().split("").filter((char) => char.match(char.match(/\p{L}|\p{N}/u))).join("");
+  length = word.length;
+  check = true;
+  for (let i = 0; i <= length / 2; i++) {
+    if (word[i] != word[length-1-i]) {
+      console.log(i, length, word[i], word[length-1-i])
+      return check = false;
+    }
+    check = true;
+  }
+  return check;
 };
+
+console.log(palindromes("abc"));
+console.log(palindromes("racecar"));
+console.log(palindromes("r3aceca3r"));
 
 // Do not edit below this line
 module.exports = palindromes;
