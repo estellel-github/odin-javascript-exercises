8d868c745efb475a0b6097d2c8be51a90ad0c31e
estellel-github <131867732+estellel-github@users.noreply.github.com>
2024-08-02 09:46:04 +0200

    Sync 09 (new version after looking at solution)

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,16 +1,26 @@
+// My attempt
+// const palindromes = function (word) {
+//   word = word.toLowerCase().split("").filter((char) => char.match(char.match(/\p{L}|\p{N}/u))).join("");
+//   length = word.length;
+//   check = true;
+//   for (let i = 0; i <= length / 2; i++) {
+//     if (word[i] != word[length-1-i]) {
+//       console.log(i, length, word[i], word[length-1-i])
+//       return check = false;
+//     }
+//     check = true;
+//   }
+//   return check;
+// };
+
+// Retrying after checking solution
+
 const palindromes = function (word) {
   word = word.toLowerCase().split("").filter((char) => char.match(char.match(/\p{L}|\p{N}/u))).join("");
-  length = word.length;
-  check = true;
-  for (let i = 0; i <= length / 2; i++) {
-    if (word[i] != word[length-1-i]) {
-      console.log(i, length, word[i], word[length-1-i])
-      return check = false;
-    }
-    check = true;
-  }
-  return check;
-};
+  reversedWord = word.split("").reverse().join("");
+  return word === reversedWord;
+}
+
 
 console.log(palindromes("abc"));
 console.log(palindromes("racecar"));
